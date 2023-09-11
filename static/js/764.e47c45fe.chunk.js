99830fba658a12a2dc6aea5619d45078ac69c822
CODEKAPP <117596036+CODEKAPP@users.noreply.github.com>
2023-09-11 00:38:52 +0000

    Deploying to gh-pages from @ CODEKAPP/goit-react-hw-05-movies@0af777c16701ca5eb09f8170ec67048f5c06fe21 🚀

--- static/js/764.e47c45fe.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[764],{764:function(e,t,u){u.r(t);var a=u(439),n=u(791),r=u(184);t.default=function(){var e=(0,n.useState)(""),t=(0,a.Z)(e,2),u=t[0],c=t[1];return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Buscar Pel\xedculas"}),(0,r.jsx)("input",{type:"text",placeholder:"Buscar pel\xedculas...",value:u,onChange:function(e){return c(e.target.value)}}),(0,r.jsx)("button",{onClick:function(){},children:"Buscar"})]})}}}]);
-//# sourceMappingURL=764.e47c45fe.chunk.js.map
